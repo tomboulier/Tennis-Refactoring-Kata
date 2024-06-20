e290ef9878c8cfef053cf42e350bbbd1cc3d5e48
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-20 14:17:10 +0200

    extract method for equality scoring

--- tennis1.py
+++ tennis1.py
@@ -9,41 +9,44 @@ class TennisGame1:
         self.p2points = 0
 
     def won_point(self, playerName):
-        if playerName == "player1":
+        if playerName == self.player1Name:
             self.p1points += 1
         else:
             self.p2points += 1
 
     def score(self):
         result = ""
-        tempScore=0
-        if (self.p1points==self.p2points):
-            result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
-                result ="Advantage player1"
-            elif (minusResult ==-1):
-                result ="Advantage player2"
-            elif (minusResult>=2):
+        if self.p1points == self.p2points:
+            result = self.equality_scoring(self.p1points)
+        elif self.p1points >= 4 or self.p2points >= 4:
+            minusResult = self.p1points - self.p2points
+            if minusResult == 1:
+                result = "Advantage player1"
+            elif minusResult == -1:
+                result = "Advantage player2"
+            elif minusResult >= 2:
                 result = "Win for player1"
             else:
-                result ="Win for player2"
+                result = "Win for player2"
         else:
-            for i in range(1,3):
-                if (i==1):
+            for i in range(1, 3):
+                if i == 1:
                     tempScore = self.p1points
                 else:
-                    result+="-"
+                    result += "-"
                     tempScore = self.p2points
                 result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
+                    0: "Love",
+                    1: "Fifteen",
+                    2: "Thirty",
+                    3: "Forty",
                 }[tempScore]
         return result
+
+    @staticmethod
+    def equality_scoring(points):
+        return {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All",
+        }.get(points, "Deuce")
