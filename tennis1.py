2ca0e0fe3daddb22f999b82aefd6ab8104323124
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-20 14:35:33 +0200

    rename variables with meaningfull names

--- tennis1.py
+++ tennis1.py
@@ -20,32 +20,32 @@ class TennisGame1:
         if self.p1points >= 4 or self.p2points >= 4:
             return self.tiebreak_scoring()
 
-        result = ""
+        score = ""
         for i in range(1, 3):
             if i == 1:
                 temporary_score = self.p1points
             else:
-                result += "-"
+                score += "-"
                 temporary_score = self.p2points
-            result += {
+            score += {
                 0: "Love",
                 1: "Fifteen",
                 2: "Thirty",
                 3: "Forty",
             }[temporary_score]
-        return result
+        return score
 
     def tiebreak_scoring(self):
-        minus_result = self.p1points - self.p2points
-        if minus_result == 1:
-            result = "Advantage player1"
-        elif minus_result == -1:
-            result = "Advantage player2"
-        elif minus_result >= 2:
-            result = "Win for player1"
+        points_difference = self.p1points - self.p2points
+        if points_difference == 1:
+            score = "Advantage player1"
+        elif points_difference == -1:
+            score = "Advantage player2"
+        elif points_difference >= 2:
+            score = "Win for player1"
         else:
-            result = "Win for player2"
-        return result
+            score = "Win for player2"
+        return score
 
     @staticmethod
     def equality_scoring(points):
